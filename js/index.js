eef1e6ea93e6e3a3fa8e4d23b7f2ae467d31b1b0
pbnhamza <pbnhamza@gmail.com>
2025-08-28 16:05:35 +0600

    first commit

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
 // icon symbol of service
+// get icon button all class
 const iconBtns = document.querySelectorAll(".icon-btn");
+//// get Navbar icon button id
 const iconNav = document.getElementById("icon");
+//click and count variable
 let count = 0;
+//loop all icons button
 for (const icon of iconBtns) {
   icon.addEventListener("click", function () {
+    //click add count
     count++;
+    //set count innerText navbar icon id
     iconNav.innerText = count;
   });
 }
@@ -22,5 +28,61 @@ for (let index = 0; index < copyBtns.length; index++) {
       copyCountNav.innerText = copyCount;
       alert("Copy Number : " + numberOfCopy);
     });
+  });
+}
+
+//Call handel
+
+const callBtns = document.getElementsByClassName("call-btn");
+
+for (let callBtn of callBtns) {
+  callBtn.addEventListener("click", function () {
+    //get service name
+    const cartBtnName =
+      callBtn.parentNode.parentNode.parentNode.childNodes[3].childNodes[1]
+        .childNodes[1].innerText;
+    console.log(cartBtnName);
+    //get service number
+    const callNumber =
+      callBtn.parentNode.parentNode.parentNode.childNodes[3].childNodes[3]
+        .childNodes[1].innerText;
+
+    //get service coin
+    const coinIcons = parseInt(document.getElementById("coin").innerText);
+
+    // alert
+    if (coinIcons <= 0) {
+      alert("❌Insufficient Coin you need 20 coin make a call ");
+      return;
+    } else {
+      alert("📞 Calling " + cartBtnName + " : " + callNumber);
+    }
+    // create object date
+    const data = [];
+    const dateObject = {
+      date: new Date().toLocaleTimeString("en-US"),
+    };
+    data.push(dateObject);
+
+    //get call history id
+    const cartHistory = document.getElementById("cart-history");
+    // create call history and show
+    const newHistory = document.createElement("div");
+    newHistory.innerHTML = `<div class="flex justify-between items-center background mt-4 p-4">
+              <div>
+                <h1 class="text-xl font-semibold text-[#111111]">${cartBtnName}</h1>
+                <p class="text-xl text-[#5C5C5C]">${callNumber}</p>
+              </div>
+              <div>
+              <p class="text-xl  text-[#111111]">${dateObject.date}</p>
+              </div>
+            </div>
+    
+    `;
+    cartHistory.append(newHistory);
+
+    //create coin icon lass par click
+    const currentCoin = coinIcons - 20;
+    document.getElementById("coin").innerText = currentCoin;
   });
 }
